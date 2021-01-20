c2466b408ef720d25a2531144b691cb7a0707a78
Guanqun Cao <guanqun.cao@volvocars.com>
2021-01-20 14:22:03 +0100

    updated get_files.py

--- download_files/get_files.py
+++ download_files/get_files.py
@@ -6,52 +6,19 @@ from time import sleep
 from pdb import set_trace
 import re
 
-def downloadlink(l):
-    f=open(l.text,"w") #perhaps you should open in a better way & ensure that file doesn't already exist.
-    br.click_link(l)
-    f.write(br.response().read())
-    print(l.text," has been downloaded")
-
-def readNames():      # general function to parse tab-delimited floats
-    fileName = "names"
-    chapters = []
-    cnt = 0
-    with open(fileName) as fr:
-        for line in fr:
-            curLine = line.strip().split()
-            chapters.append(curLine[0])
-    # print chapters
-    chaptersFull = []
-    header = "http://www.cs.princeton.edu/~wayne/kleinberg-tardos/pdf/"
-    for i in range(len(chapters)):
-        if i < 10:
-            chaptersFull.append(header + "0" + str(i) + chapters[i] +  "-2x2.pdf")
-        elif i >= 10:
-            chaptersFull.append(header + str(i) + chapters[i] + "-2x2.pdf")
-    print(chaptersFull)
-    return chapters
-
 if __name__ == '__main__':
-    root_path = "/home/gcao/tmp/"
+    root_path = "/home/gcao/lectures/bayes_filter/"
     br = mechanicalsoup.StatefulBrowser()
     # Open your site
-    br.open("http://www.cs.princeton.edu/~wayne/kleinberg-tardos/pdf/")
-    suffix = [".xlsx"] #you will need to do some kind of pattern matching on your files
+    br.open("https://mycourses.aalto.fi/course/view.php?id=24745&section=2")
+    prefix = ["https://mycourses.aalto.fi/mod/resource/view.php?id="]
     myfiles = []
-    print(br.links())
     for l in br.links(): #you can also iterate through br.forms() to print forms on the page!
-        for t in suffix:
+        for t in prefix:
             if t in str(l): 
                 myfiles.append(l)
     base_url = br.get_url() 
-    #base_url = re.sub('lecture_slides.html', '', br.get_url())
     for l in myfiles:
-        # sleep(5) #throttle so you dont hammer the site
-        # downloadlink(l)
-        #command = "cd "+ root_path +"; " +  "wget " + l.attrs['href']
-        command = "cd "+ root_path +"; " +  "wget " + base_url + l.attrs['href']
+        command = "cd "+ root_path +"; " +  "curl -O -J -L " + l.attrs['href'] # redirect links to real filenames
         subprocess.call(command, shell=True)
         print("Downloaded " + l.attrs['href'])
-        print(l.text)
-    # readNames()
-    # subprocess.call("cd ~/bash_code; sh pltr_batch2.sh", shell=True)    
