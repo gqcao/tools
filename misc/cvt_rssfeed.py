d07bee1ab21a209db87e16057358dfeadf42e8cb
Guanqun Cao <guanqun.cao@ieee.org>
2024-03-14 16:33:39 +0100

    added one file to convert rss feed from xml to html

---
+++ misc/cvt_rssfeed.py
@@ -0,0 +1,34 @@
+#!/bin/python
+
+import numpy as np
+import re
+
+def get_line_indent(line):
+    """Returns the number of leading spaces in a line."""
+    return len(line) - len(line.lstrip())
+
+file_path = 'rss_20240221.xml'
+curr_tag = ""
+pattern = r'(\w+)\s*=\s*"([^"]+)"'
+urls= []
+
+with open(file_path, 'r') as file:
+    for line_num, line in enumerate(file, start=1):
+        # Calculate the indent of the current line
+        indent = get_line_indent(line)
+        
+        matches = re.findall(pattern, line)
+        # Constructing dictionary from extracted key-value pairs
+        content = {key: value for key, value in matches}
+
+        if indent == 4 and content.get("title") is not None:
+            curr_tag = content["title"]
+        if content.get("xmlUrl"):
+            urls.append(content.get("xmlUrl") + " " + curr_tag)
+
+wrt_file_path = 'urls'
+
+# Open the file in write mode and write each element of the list to a new line
+with open(wrt_file_path, 'w') as file:
+    for item in urls:
+        file.write(item + '\n')
