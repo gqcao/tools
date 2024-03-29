a484470da930c45c3a99defc205a7b063a80fd28
Guanqun Cao <guanqun.cao@ieee.org>
2024-03-29 17:22:33 +0100

    refine download

--- download_files/yt_dl.sh
+++ download_files/yt_dl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-YT_URL=https://www.youtube.com/watch?v=8dcJ-U1dUx0
+YT_URL=
 
 nohup youtube-dl --extract-audio --audio-format mp3 $YT_URL 1> 00dl_mp3.out 2> 00dl_mp3.err &
 #nohup youtube-dl $YT_URL 1> 00dl_mp3.out 2> 00dl_mp3.err &
