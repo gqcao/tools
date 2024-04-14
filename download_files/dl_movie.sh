638f90f78f65a9bf417628ac5f20cf2d355dd30e
Guanqun Cao <gqcaoai@gmail.com>
2024-04-14 22:02:50 +0200

    update download

--- download_files/dl_movie.sh
+++ download_files/dl_movie.sh
@@ -2,6 +2,6 @@
 
 DL_PATH=$HOME/Downloads/videos
 TORRENT_PATH=$HOME/Downloads
-TORRENT_FILE=boy.torrent
+TORRENT_FILE=foo.torrent
 
 nohup transmission-cli -u 1 -w $DL_PATH $TORRENT_PATH/$TORRENT_FILE 1> 00vid.out 2> 00vid.err &
