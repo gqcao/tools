2c61f6020ccdfca7708740a740363f58619e416d
Guanqun Cao <gqcaoai@gmail.com>
2025-03-18 19:20:32 +0100

    updated get_images.py

--- download_files/get_images.py
+++ download_files/get_images.py
@@ -1,25 +1,20 @@
-import re
 import requests
 from bs4 import BeautifulSoup
 import subprocess
+import time
 
 root_path = '/home/gcao/tmp/pics'
-site = "https://yle.fi/a/74-20082473"
+site = "https://chedongxi.com/p/337204.html"
 
 response = requests.get(site)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 img_tags = soup.find_all('img')
-urls = []
-yle_base_url = "https://images.cdn.yle.fi/image/upload/"
-for img in img_tags:
-    if img.has_attr('src'):
-        urls.append(yle_base_url + img['src'].split("/")[-1])
+
 cnt = 0
-for url in urls:
-    # sleep(5) #throttle so you dont hammer the site
-    # downloadlink(l)
-    command = "cd "+ root_path +"; " +  "wget " + url + ' -O ' + str(cnt).zfill(5) + '.jpg'
-    #command = "cd "+ root_path +"; " +  "wget " + base_url + l.attrs['href']
+for img in img_tags:
+    print("Currently downloaded: {}".format(img['src']))
+    time.sleep(2)
+    command = "cd "+ root_path +"; " +  "curl -o " + str(cnt).zfill(3) + '.jpg ' + img['src']
     cnt = cnt + 1
     subprocess.call(command, shell=True)
